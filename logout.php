0168f4d5636202dc2f70cf39848210c68d016354
OpenShift System User <5605b6200c1e660f6d000117@ex-std-node449.prod.rhcloud.com>
2015-12-09 01:30:53 -0500

    perfecto

--- logout.php
+++ logout.php
@@ -1,10 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 include './include/sign_in.php';
 
 if(sesion_iniciada()){
